7526c82e2545e015d9be4b37294484992c19e26a
bitski <18152029+bitski@users.noreply.github.com>
2025-11-13 15:03:23 +0100

    feat(forms): enhance contact form with input fields and submit button

--- templates/components/forms/contact/form.php
+++ templates/components/forms/contact/form.php
@@ -13,5 +13,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <form method="post" class="contact-form" action="">
     <?php wp_nonce_field( 'contact_form', 'contact_form_nonce' ); ?>
-    Form
+
+    <div class="input-group mb-3">
+        <label for="contact-name" class="visually-hidden">Name</label>
+        <input id="contact-name" class="form-control" name="contact_name" type="text"  placeholder="Dein Name" required>
+        <div class="invalid-feedback">Bitte gib deinen Namen ein.</div>
+    </div>
+
+    <button type="submit" class="btn btn-primary mt-3">
+        <i class="fa-solid fa-paper-plane me-2" aria-hidden="true"></i>
+        Nachricht senden
+    </button>
 </form>
--- templates/components/search/form.php
+++ templates/components/search/form.php
@@ -10,16 +10,17 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-$class = !empty($args['class']) ? $args['class'] : '';
+$class = ! empty( $args['class'] ) ? $args['class'] : '';
 ?>
 
-<form role="search" method="get" class="search-form <?php echo esc_attr($class); ?>" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form role="search" method="get" class="search-form <?php echo esc_attr( $class ); ?>"
+      action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <label for="search-bar-input" class="visually-hidden">Suche</label>
     <div class="input-group">
-        <input type="search" id="search-bar-input" class="search-bar-input form-control"
-               placeholder="Suchen …" value="<?php echo get_search_query(); ?>" name="s">
-        <button type="submit" class="search-bar-submit btn btn-outline-secondary"
-                aria-label="Suche absenden">
+        <input id="search-bar-input" class="search-bar-input form-control" type="search" name="s"
+               value="<?php echo get_search_query(); ?>"
+               placeholder="Suchen …">
+        <button class="search-bar-submit btn btn-outline-secondary" type="submit" aria-label="Suche absenden">
             <i class="fa-solid fa-magnifying-glass" aria-hidden="true"></i>
         </button>
     </div>
